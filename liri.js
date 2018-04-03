e1ccd75dcd1dc8498bd00da0c9946bc55b02016f
ntdang <ndang83@gmail.com>
2018-04-03 11:53:15 -0400

    Added switch case

--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
+//Include packages and keys
 require('dotenv').config();
 var Spotify = require('node-spotify-api');
 var request = require('request');
-var keysINeed = require("./keys.js");
-console.log(keysINeed);
+var keys = require("./keys.js");
+console.log(keys);
 
 console.log(process.env.SPOTIFY_CLIENTID);
 console.log(process.env.SPOTIFY_CLIENTSECRET);
@@ -15,3 +16,56 @@ var spotify = new Spotify({
   id: process.env.SPOTIFY_CLIENTID,
   secret: process.env.SPOTIFY_CLIENTSECRET
 });
+
+var input = process.argv[2];
+
+switch (input) {
+  case 'my-tweets':
+    getTweets();
+    break;
+  case 'spotify-this-song':
+    getSong();
+    break;
+  case 'movie-this':
+    getMovieInfo();
+    break;
+  case 'do-what-it-says':
+    justDoIt();
+    break;
+  default:
+    console.log("You didn't put in a proper input. Please use 'my-tweets', 'spotify-this-song', 'movie-this',or 'do-what-it-says'.");
+    break;
+};
+
+//OMDB function
+function getMovieInfo() {
+  // Grab or assemble the movie name and store it in a variable called "movieName"
+  var movieName = process.argv.slice(2).join(' ');
+
+  // Then run a request to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+  // This line is just to help us debug against the actual URL.
+  console.log(queryUrl);
+
+  // Then create a request to the queryUrl
+  // ...
+
+  // If the request is successful
+  // ...
+
+  // Then log the Release Year for the movie
+  // ...
+
+  /*request(urlBeingRequested, callback(err, responseObj, dataReturned))*/
+
+  request(queryUrl, function (err, response, data) {
+    if (!err && response.statusCode === 200) {
+      console.log(data);
+      console.log('=============');
+      console.log(JSON.parse(data));
+      console.log('=============');
+      console.log(JSON.parse(data).Title);
+    }
+  });
+};
