540cc9c6fd31567af06426e7a5c94e767f2f276e
ntdang <ndang83@gmail.com>
2018-04-03 13:08:34 -0400

    Spotify function sort of works

--- liri.js
+++ liri.js
@@ -40,7 +40,9 @@ switch (input) {
 
 //Twitter function
 function getTweets() {
+  //Grab username and store it in a variable called "params"
   var params = {screen_name: 'itsameND'};
+  //Run a request to Twitter API
   client.get('statuses/user_timeline', params, function(err, tweets, response) {
     if (err) {
       return console.log(err);
@@ -51,7 +53,15 @@ function getTweets() {
 
 //Spotify function
 function getSong() {
-
+  //Grab song name and store it in a variable called "songName"
+  var songName = process.argv.slice(3).join(' ');
+  //Run request to Spotify API
+  spotify.search({ type: 'track', query: songName, limit: 5}, function(err, data) {
+    if (err) {
+      return console.log(err);
+    };
+    console.log(data);
+  });
 };
 
 //OMDB function
