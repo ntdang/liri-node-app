c219bdc294c6c21ff9e12bd5ed952ca884de131f
ntdang <ndang83@gmail.com>
2018-04-03 10:04:51 -0400

    Fixed keys.js

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.uJ07qPxxwhjajYb9IRoTAIIdS,
-  consumer_secret: process.env.DuLqShwYaT6sTLcDRDRyMSUpxTEOHvKQLCi83HtX7CbCqxIaP2,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY, //980841916760772609-lvFKkCc9To68yfy75FIBa94x6OvPsOj
-  access_token_secret: process.env.aFs4Yk8C8wwfY7sygaZ7i37gTKAQIBRYmSNm9tgC508Oy
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.fc263820fb924975ad1acb0f806524bc,
-  secret: process.env.SPOTIFY_SECRET //12a9dba95017401fb65ce66a436cd0c8
+  id: process.env.SPOTIFY_CLIENTID,
+  secret: process.env.SPOTIFY_CLIENTSECRET
 };
